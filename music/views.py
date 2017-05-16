21554f7ad6e430abe6584ced0b16d3e2b68c3459
Monal Shadi <monalshadi16@gmail.com>
2017-05-17 00:59:44 +0530

    Raised Http404 for invalid links

--- music/views.py
+++ music/views.py
@@ -1,16 +1,16 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    return HttpResponse(template.render(context, request))
-
+    return render(request, 'music/index.html', {'all_albums': all_albums})
+    # We can use context = {'all_albums': all_albums}, but as its used only once, lets condense
 
 def detail(request, album_id):
-    return HttpResponse('<h2>Detail for Album id: ' + str(album_id) + '</h2>')
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404('Album Does not exits')
+    return render(request, 'music/detail.html', {'album': album})
