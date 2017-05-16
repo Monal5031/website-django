7df365069c906613fd518972f58d450e7f56ebed
Monal Shadi <monalshadi16@gmail.com>
2017-05-17 00:27:40 +0530

    HTML Template added

--- music/urls.py
+++ music/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # /music/71/
-    url(r'^(?P<album_id>[0-9]+)$', ),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
 
 
 def index(request):
-    return HttpResponse('<h1>This is the Music app homepage</h1>')
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, album_id):
-    return HttpResponse('<h2>Detail for Album </h2>')
+    return HttpResponse('<h2>Detail for Album id: ' + str(album_id) + '</h2>')
