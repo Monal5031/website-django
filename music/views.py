66aa6d1650a22c68fd7febe04cca4a49adcec5a9
Monal Shadi <monalshadi16@gmail.com>
2017-05-16 23:05:36 +0530

    New updates

--- music/views.py
+++ music/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-	return HttpResponse('<h1>This is the Music app homepage</h1>')
+    return HttpResponse('<h1>This is the Music app homepage</h1>')
+
+
+def detail(request, album_id):
+    return HttpResponse('<h2>Detail for Album </h2>')
