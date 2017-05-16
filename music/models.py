766462c52aaeb69ed1245f698fceddf621048e72
Monal Shadi <monalshadi16@gmail.com>
2017-05-17 01:36:40 +0530

    Added songs to music page

--- music/models.py
+++ music/models.py
@@ -16,4 +16,7 @@ class Song(models.Model):
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.song_title
+
 
