a8414791e936380a5c6c002840c6528453357578
Monal Shadi <monalshadi16@gmail.com>
2017-05-19 02:35:13 +0530

    Added form for albums. add/update/delete album. Css fixed

--- music/models.py
+++ music/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Album(models.Model):
@@ -6,6 +7,9 @@ class Album(models.Model):
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
+
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.album_title + ' - ' + self.artist
