290c44add71b30040dc38826b959728f0039a7de
Monal Shadi <monalshadi16@gmail.com>
2017-05-18 14:28:55 +0530

    Made views generic

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,8 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-
-    # /music/<album_id>/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
